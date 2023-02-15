17b51baee097b9147e5a1eff5962800d324dc6d0
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-02-15 11:16:42 +0530

    dbdata

---
+++ mghealthcare/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+// async function  getdata(){
+//    let a = await fetch(`http://localhost:8080/featured_brands`)
+//    let b = await a.json()
+//    console.log(b)
+//  }
+// useEffect(() => {
+//   getdata()
+// }, [])
+
+ return (
+    <div className="App">
+         <h1> mg store </h1>
+       
+         
+    </div>
+  );
+}
+
+export default App;
